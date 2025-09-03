dbe4c9244cdf80ed324f10910ae8f1be520a3629
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-09-03 23:07:59 +0530

    update my project structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,19 @@
+import Logo from "../assets/logo.svg";
+import './header.css';
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <a href="/" className="logo">
+        <img src={Logo} alt="Routemate Logo" />
+        <span>Routemate</span>
+      </a>
+      <nav className="navigation">
+        <a href="/" className="link">Home</a>
+        <a href="/products" className="link">Products</a>
+        <a href="/contact" className="link">Contact</a>
+      </nav>
+    </header>
   )
 }
 
